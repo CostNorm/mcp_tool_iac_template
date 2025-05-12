725e8c798cab22aba38ae20965c9dc12cd0731c3
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2025-05-12 15:48:54 +0900

    cloudtrail 정책 업데이트

--- iam.tf
+++ iam.tf
@@ -84,3 +84,9 @@ resource "aws_iam_role_policy_attachment" "efs_policy_attachment" {
   role = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "cloudtrail_policy_attachment" {
+  count = var.attach_cloudtrail_policy ? 1 : 0
+  role = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrail_FullAccess"
+}
